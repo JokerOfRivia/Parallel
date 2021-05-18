03dccc7c030be59a4ea2719134e259cfcb03e398
JokerOfRivia <70218072+JokerOfRivia@users.noreply.github.com>
2021-05-18 09:31:18 -0700

    Files

--- Parallel.java
+++ Parallel.java
@@ -1,13 +1,27 @@
 import java.util.*;
 import java.io.*;
 public class Parallel implements Runnable {
-  public static void main(String[] args) {
-    Parallel obj = new Parallel();
-    Thread thread = new Thread(obj);
-    thread.start();
-    System.out.println("This code is outside of the thread");
+  public static void main (String [] args){
+    AccountReader.readfile("/Users/nathanthomasmacbookpro2018/Documents/GitHub/Parallel/Accounts/Accounts 0.txt");
   }
-  public void run() {
-    System.out.println("This code is running in a thread");
-  }
+  public static void readFile (String filename){
+        double maxNum = 0;
+        try{
+            File myObj = new File(filename);
+              Scanner myReader = new Scanner(myObj);
+              while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                //System.out.println (data);
+                //DO STUFF HERE
+                String numWord = data.substring (16,data.length());
+                double num = Double.parseDouble (numWord);
+
+
+              }
+              myReader.close();
+        }catch (FileNotFoundException e) {
+              System.out.println("An error occurred. Ha nerd.");
+              e.printStackTrace();
+          }
+    }
 }
